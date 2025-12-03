aa2b7c839803216b6914bf5f52ddcdc44e451d1a
JanGabrielqqq <jangabrielqqq@gmail.com>
2025-12-03 14:14:04 +0800

    UI changes
    - scrollbar css
    - remove portfolio header
    - change background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/header"
 import HeroSection from "@/components/hero-section"
 import SocialLinks from "@/components/social-links"
 import About from "@/components/about"
@@ -9,8 +8,7 @@ import Footer from "@/components/footer"
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <Header />
-      <main className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-4xl">
+      <main className="container pt-18 mx-auto px-4 sm:px-6 lg:px-8 max-w-4xl">
         <HeroSection />
         <SocialLinks />
         <About />
